72b1166532b08dd8b0273d0dc642d3d351f10afe
dayanell <dayane.ll@outlook.com.br>
2023-06-10 20:08:05 -0300

    config issue fixed project

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "1t7est",
+  projectId: "zpp7sm",
   env: {
     url: "https://vue-vuex-realworld.netlify.app/#/"
   },
