19a847dab990ce8783c7b6b707f855ca4b688e68
dayanell <dayane.ll@outlook.com.br>
2023-06-10 19:45:40 -0300

    config folder issue

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
 
-module.exports = defineConfig({
+export default defineConfig({
   defaultCommandTimeout: 60000,
   projectId: "1t7est",
 
